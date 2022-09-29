257c0bd306805e91960d4e638f620781dee37e59
Lahuts <albumcroquis@gmail.com>
2022-09-29 12:53:23 +0200

    AlternateJS update

--- js/app.js
+++ js/app.js
@@ -1,39 +1,22 @@
 document.addEventListener("DOMContentLoaded", e => {
-    
-    class ToggleItem{
-        constructor(img,pnl){
-            this.img = img;
-            this.pnl = pnl;
-            this.onOff = true;
-            this.ajoutEvent(img,pnl);
-        };
-        setOnOff(onOff){
-            this.onOff = onOff;
-        }
-        changeAttrib(pnl,type,value){
-            pnl.setAttribute(type,value);
-        }
-        ajoutEvent(img,pnl){
-            img.addEventListener("click",()=>{
-                if(this.onOff){
-                    this.changeAttrib(pnl,"class","toggle-on");
-                    this.setOnOff(false);
-                    this.img.src = "asset/remove.svg";
-
+    let img = document.querySelectorAll(".visible-pannel");
+    let toggle = document.querySelectorAll(".toggle-pannel");
+    for(i = 0; i < img.length; i++) {
+        let u = {
+            d: i,
+            c: document.querySelectorAll(".visible-pannel img"),
+            b:true,
+            a: img[i].addEventListener("click",()=>{
+                if(u.b){
+                    toggle[u.d].setAttribute("class","toggle-on");
+                    u.c[u.d].src = "asset/remove.svg";
+                    u.b = false;
                 }else{
-                    this.changeAttrib(pnl,"class","toggle-pannel");
-                    this.setOnOff(true);
-                    this.img.src = "asset/cross.svg";
+                    toggle[u.d].setAttribute("class","toggle-pannel");
+                    u.c[u.d].src = "asset/cross.svg";
+                    u.b = true;
                 }
-                
-            });
+            }),
         }
     }
-
-    let img = document.querySelectorAll("img");
-    let pnl = document.querySelectorAll(".toggle-pannel");
-    for(i = 0;i<img.length;i++){
-        let Itms = new ToggleItem(img[i], pnl[i]);
-    }
-
 });
